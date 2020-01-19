c6f9bcd0e635a403b7df56fb0f725e7f67287642
Shubham Sinha <sinhashubham24@gmail.com>
2020-01-19 14:17:33 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,3 +5,20 @@ if (workbox) {
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
+
+workbox.routing.registerRoute(
+  /\.js$/,
+  new workbox.strategies.NetworkFirst()
+);
+
+workbox.routing.registerRoute(
+  // Cache CSS files.
+  /\.css$/,
+  // Use cache but update in the background.
+  new workbox.strategies.StaleWhileRevalidate({
+    // Use a custom cache name.
+    cacheName: 'css-cache',
+  })
+);
+
+
